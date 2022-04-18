94ceae263bb0a593b17a978ff6587717a5eba0e6
Abhay Korat <abhaykorat89@gmail.com>
2022-04-18 19:03:46 +0530

    Lexical
    Lexical or Preprocessor Issue (Xcode): 'whatsapp_share/whatsapp_share-Swift.h' file not found

--- ios/Classes/whatsapp_share2Plugin.m
+++ ios/Classes/whatsapp_share2Plugin.m
@@ -1,5 +1,5 @@
 #import "whatsapp_share2Plugin.h"
-#import <whatsapp_share/whatsapp_share-Swift.h>
+#import <whatsapp_share2/whatsapp_share2-Swift.h>
 
 @implementation WhatsappShare
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
